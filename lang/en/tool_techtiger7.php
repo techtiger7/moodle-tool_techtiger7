7f21b681a16a2dd99abe72db5339efe3651141fd
Tom <twdickman@gmail.com>
2019-01-10 17:26:04 +0700

    Added missing comment to string file

--- lang/en/tool_techtiger7.php
+++ lang/en/tool_techtiger7.php
@@ -1,2 +1,27 @@
 <?php
-$string['techtiger7'] = 'My first Moodle plugin';
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'techtiger7', language 'en', branch 'MOODLE_35_STABLE'
+ *
+ * @package tool_techtiger7
+ * @copyright 2018, Tom Dickman <twdickman@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'My first Moodle plugin';
